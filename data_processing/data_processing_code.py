e8dc87c0a1945f7f53de66143476b6b4b6d2bdc5
You_Tian <ye.yaxin3@gmail.com>
2020-03-23 22:39:14 -0400

    update ffill

--- data_processing/data_processing_code.py
+++ data_processing/data_processing_code.py
@@ -1,3 +1,6 @@
+import pandas as pd
+import numpy as np
+#%%
 '''Calculate n month diffrence transformation for a variable
 For exmample, when conducting feature engineering for machine learning models, we
 can use the code below to find the transformation for n month difference of a feature'''
@@ -9,8 +12,7 @@ df.sort_values(by=['date'], inplace = True, acending = True)
 diff_number = 3
 df[new_name] = df.groupby(['CC'])[column_name].apply(lambda x:x.diff(diff_number))
 
-
-
+#%%
 '''Calculate n month rolling average transformation for a variable
 For exmample, when conducting feature engineering for machine learning models, we
 can use the code below to find the transformation for n month rolling average of a feature'''
@@ -18,3 +20,15 @@ can use the code below to find the transformation for n month rolling average of
 df.sort_values(by=['date'], inplace = True, acending = True)
 rolling_months = 6
 df[new_name] = df.groupby(['CC'])[column_name].apply(lambda x:x.rolling(window=rolling_months).mean())
+
+#%%
+# Forward Fill NA value for all NA, eg fill vallues between March and July if there are N/A in April, May
+# and June. The forward fill processing may take a long time to finish
+df.sort_values(by=['date'], inplace = True, acending = True)
+# Group by CC and on the CC level, for each CC with different dates, forward fill them:
+for cocc in df['CC'].unique():
+    df[df['cc'] == cocc] = df[df['cc'] == cocc].ffill()
+
+#%%
+# Resample technique using bootstrap
+# In machine learning 
